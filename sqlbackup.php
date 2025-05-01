74eab45b5df6eb260498f1c12c6d9b5e4567894a
stievenk <stieven.kalengkian@gmail.com>
2025-05-01 08:59:16 +0800

    First commit

---
+++ sqlbackup.php
@@ -0,0 +1,62 @@
+<?php
+use Koyabu\MysqlBackup\Backup;
+
+/**
+ * Note:
+ * You need to set your `mysqldump` execution to Global
+ * or you can set mysqldump path location
+ * Linux Example:
+ * $Backup->setMysqlDumpPath('/usr/bin/');
+ * Windows Example:
+ * $Backup->setMysqlDumpPath('C:\\Program Files\\MySQL\\MySQL Server 5.7\\bin\\');
+ * 
+ * You can run this script with cron job or task scheduler
+ * set to run everyday (see cron manual or task scheluder manual):
+ * /usr/bin/php sqlbackup.php > /dev/null 2>&1 &
+ * 
+ * For Windows recommended use .bat file
+ */
+$config = array();
+$config['mysql']['host']="localhost";
+$config['mysql']['user']="root";
+$config['mysql']['pass']="";
+$config['mysql']['port']="3306";
+$config['mysql']['data']="test";
+
+include 'vendor/autoload.php';
+
+$Backup = new Backup($config);
+$Backup->skipAlways(['performance_schema','mysql','test']);
+
+/*
+Set Dir where file backup will be saved
+*/
+$Backup->setDir('./data/sqlbackup/');
+
+// Backup Daily for specific database
+$Backup->Daily(['aknj']);
+// if parameter = null -> this will backup all database; alias $Backup->All();
+
+// Backup Monthly specific database every date 28 of month
+$Backup->Monthly(['wagw'],28);
+
+// Backup Every week on Thursday (0 = Sunday, 6 = Saturday)
+$Backup->Weekly(['hwm'],4);
+
+/*
+* Example: All DB Backup
+* $Backup->All(); 
+* this is Alias for $Backup->Daily(); without parameter
+*
+* or you can backup all database with filter parameter:
+* $Backup->All($arrayDB_toSkip);
+*
+* Example: All DB Backup except aknj and wagw
+* $Backup->All(['aknj','wagw']);
+*/
+//
+
+$file_prefix = '';
+$file_sufix = '_'.date("Y-m-d");
+$Backup->run($file_prefix, $file_sufix, true);
+?>
---
+++ src/Backup.php
@@ -0,0 +1,105 @@
+<?php
+namespace Koyabu\mysqlbackup;
+
+class Backup {
+
+   public $BASE_DIR = './data/sqlbackup/';
+   private $conn;
+   public $Databases;
+   public $DB_Backup = [];
+   public $mysqldumppath = '';
+   public $skipDB = [];
+
+   function __construct($config) {
+      $this->conn = new \mysqli($config['mysql']['host'],$config['mysql']['user'],$config['mysql']['pass'],$config['mysql']['data']);
+      $this->getAllDatabases();
+      $this->createDir();
+   }
+
+   function setDir($dir) {
+      $this->BASE_DIR = $dir;
+      $this->createDir();
+   }
+
+   function createDir($subdir='') {
+      if (!file_exists($this->BASE_DIR.$subdir)) {
+         mkdir($this->BASE_DIR,0777,true);
+      }
+   }
+
+   function getAllDatabases() {
+      $g = $this->conn->query("show databases;");
+      while($t = $g->fetch_assoc()) {
+         $this->Databases[] = $t['Database'];
+      }
+   }
+
+   function Daily($databases = []) {
+      if (count($databases) > 0) {
+         $this->DB_Backup = array_merge($this->DB_Backup,$databases);
+         $this->DB_Backup = array_unique($this->DB_Backup);
+         sort($this->DB_Backup);
+      } else {
+         $this->DB_Backup = $this->Databases;
+      }
+   }
+
+   function Weekly($databases = [], $weekday = 1) {
+      if (date("w") == $weekday) {
+         $this->DB_Backup = array_merge($this->DB_Backup,$databases);
+         $this->DB_Backup = array_unique($this->DB_Backup);
+         sort($this->DB_Backup);
+      }
+   }
+
+   function Monthly($databases = [], $montlhyday = 1) {
+      if (date("d") == $montlhyday) {
+         $this->DB_Backup = array_merge($this->DB_Backup,$databases);
+         $this->DB_Backup = array_unique($this->DB_Backup);
+         sort($this->DB_Backup);
+      }
+   }
+
+   function All($filter = []) {
+      if (count($filter) > 0) {
+         $DBS = [];
+         for ($i = 0; $i < count($this->Databases); $i++) {
+            if (!in_array($this->Databases[$i], $filter)) {
+               $DBS[] = $this->Databases[$i];
+            }
+         }
+         $this->DB_Backup = array_merge($this->DB_Backup,$DBS);
+      } else {
+         $this->DB_Backup = $this->Databases;
+      }
+   }
+
+   function skipAlways($db) {
+      $this->skipDB = $db;
+   }
+
+   function setMysqlDumpPath($path) {
+      $this->mysqldumppath = $path;
+   }
+
+   function dump($Database,$fileoutput,$debug = false) {
+      $exec = "{$this->mysqldumppath}mysqldump -uroot ". ( $this->config['mysql']['pass'] ? "-p{$this->config['mysql']['pass']}" : '')  ." {$Database} > {$this->BASE_DIR}{$fileoutput}.sql";
+      if ($debug) { echo $exec.PHP_EOL; }
+      exec($exec,$retr);
+      if ($debug) {
+         if ($retr) { print_r($retr); }
+      }
+      return "{$this->BASE_DIR}{$Database}.sql";
+   }
+
+   function run($file_prefix = '', $file_sufix ,$debug = false) {
+      for ($i = 0; $i < count($this->DB_Backup); $i++) {
+         if (in_array($this->DB_Backup[$i], $this->skipDB)) {
+            continue;
+         }
+         $this->dump($this->DB_Backup[$i],$file_prefix.$this->DB_Backup[$i].$file_sufix,$debug);
+      }
+   }
+
+}
+?>

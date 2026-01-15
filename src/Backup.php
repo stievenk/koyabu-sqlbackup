bbbc8c1ce7e743930bda0ce9cec4dad8157d1069
Stieven Kalengkian <stieven.kalengkian@gmail.com>
2026-01-15 16:16:55 +0800

    Fix SendTelegram

--- src/Backup.php
+++ src/Backup.php
@@ -114,7 +114,7 @@ class Backup {
 
    function run($file_prefix = '', $file_sufix = '' ,$debug = false) {
       $limit = $this->config['keepFile_count'] ?? 3;
-
+      $dbCount = 0; $cloudSync = []; $rm = 0; $filesize=0; $dbs = [];
       foreach($this->DB_Backup as $Database) {
          if (in_array($Database, $this->skipDB)) {
             continue;
@@ -130,7 +130,8 @@ class Backup {
                'created_at' => date("Y-m-d H:i:s")
             ];
          }
-
+         $dbCount++;
+         $dbs[] = $Database;
          $file = $this->dump($Database,$savePath.$file_prefix.$Database.$file_sufix);
          if ($this->config['zip']['compress'] == true) {
             if (!empty($this->config['zip']['exec'])) {
@@ -139,18 +140,32 @@ class Backup {
                $file = $file.'.bz2';
             }
          }
+         $filesize += filesize($file);
          $cfg['filename'][] = basename($file);
          $cfg['last_update'] = date("Y-m-d H:i:s");
          $cfg['filename'] = array_unique($cfg['filename']);
          echo $savePath.PHP_EOL;
          file_put_contents($savePath.'backup.json',json_encode($cfg,JSON_PRETTY_PRINT));
-         $this->removeFile($Database,$limit);
-         $this->cloudSync($Database,$file);
+         $rm += $this->removeFile($Database,$limit);
+         $cs = $this->cloudSync($Database,$file);
+         $cloudSync['dbx_sync'] += $cs['dbx_sync'];
+         $cloudSync['dbx_remove'] += $cs['dbx_remove'];
+         $cloudSync['gdrive_sync'] += $cs['gdrive_sync'];
+         $cloudSync['gdrive_remove'] += $cs['gdrive_remove'];
       }
+
+      return [
+         'cloud' => $cloudSync,
+         'db' => $dbCount,
+         'local_remove' => $rm,
+         'total_size' => round($filesize / 1024 / 1024, 2) . ' MB',
+         'dbs' => $dbs
+      ];
    }
 
    function removeFile($Database,$limit = 3) {
       $savePath = trim($this->BASE_DIR,'/') .'/'. $Database .'/';
+      $rm = 0;
       if (!file_exists($savePath.'backup.json')) {
          return false;
       }
@@ -163,6 +178,7 @@ class Backup {
          for ($i = 0; $i < count($cfg['filename']) - $limit; $i++) {
             echo "REMOVE: ".$savePath . $cfg['filename'][$i].PHP_EOL;
             unlink($savePath . $cfg['filename'][$i]);
+            $rm++;
          }
          print_r($saveFile);
          $cfg['filename'] = $saveFile;
@@ -170,12 +186,16 @@ class Backup {
       $cfg['filename'] = array_unique($cfg['filename']);
       $cfg['last_update'] = date("Y-m-d H:i:s");
       file_put_contents($savePath.'backup.json',json_encode($cfg,JSON_PRETTY_PRINT));
+      return $rm;
    }
 
    public function cloudSync($Database,$file) {
       $limit = $this->config['keepFile_count'] ?? 3;
       $savePath = trim($this->BASE_DIR,'/') .'/'. $Database .'/';
       $cfg = json_decode(file_get_contents($savePath.'backup.json'),true);
+
+      $data = [];
+
       if (!file_exists($savePath.'backup.json')) {
          return false;
       }
@@ -185,6 +205,7 @@ class Backup {
             print_r($res);
             $cfg['dropbox']['last_update'] = date("Y-m-d H:i:s");
             $cfg['dropbox']['files'][] = $res;
+            $data['dbx_sync']++;
          }
       }
       // Remove Old File Dropbox
@@ -193,6 +214,7 @@ class Backup {
             $r = $this->DropboxDelete($cfg['dropbox']['files'][$i]['path_lower']);
             print_r($r);
             echo "REMOVE: ".$cfg['dropbox']['files'][$i]['path_lower'].PHP_EOL;
+            $data['dbx_remove']++;
          }
          $cfg['dropbox']['files'] = array_slice($cfg['dropbox']['files'],-$limit);
       }
@@ -201,6 +223,7 @@ class Backup {
          $res = $this->GoogleDriveSync($file);
          $cfg['gdrive']['last_update'] = date("Y-m-d H:i:s");
          $cfg['gdrive']['files'][] = $res;
+         $data['gdrive_sync']++;
       }
 
       // Remove Old File Google Drive
@@ -209,11 +232,13 @@ class Backup {
             $r = $this->GoogleDriveDelete($cfg['gdrive']['files'][$i]['id']);
             print_r($r);
             echo "REMOVE: ".$cfg['gdrive']['files'][$i]['name'].PHP_EOL;
+            $data['gdrive_remove']++;
          }
          $cfg['gdrive']['files'] = array_slice($cfg['gdrive']['files'],-$limit);
       }
 
       file_put_contents($savePath.'backup.json',json_encode($cfg,JSON_PRETTY_PRINT));
+      return $data;
    }
 
    public function DropboxSync($filename) {
@@ -315,6 +340,15 @@ class Backup {
       $drive = new GoogleDriveClient($this->config['gdrive']);
       echo "Remove file {$id} from Google Drive\n";
       $r = $drive->delete($id);
+      return $r;
+   }
+
+   function sendTelegram($text) {
+      if (empty($this->config['telegram'])) { return false; }
+      if (empty($this->config['telegram']['chat_id'])) { return false; }
+      $bot = new Telegram($this->config['telegram']);
+      $r = $bot->send($this->config['telegram']['chat_id'],$text);
+      print_r($r);
       return $r;
    }
 }
